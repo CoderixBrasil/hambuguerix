7d9fb707ecc3c4b7f592e453a4341ced27eaa80b
ghianluca <ribeiroghian@gmail.com>
2024-03-15 19:39:39 -0300

    feat: create welcome screen

--- app/View/Mobile/index.php
+++ app/View/Mobile/index.php
@@ -1,15 +1,27 @@
+<?php
+
+?>
+
 <!DOCTYPE html>
 <html lang="pt-BR">
 
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Boas-vindas!</title>
+    <link rel="stylesheet" href="index.css">
+    <title>Hamburguerix - Boas-vindas</title>
 </head>
 
 <body>
-
-
+    <main>
+        <div class="top-section">
+            <h1>Boas-vindas!</h1>
+            <button>Levar para Viagem</button>
+        </div>
+        <div class="bottom-section">
+            <button>Comer Aqui</button>
+        </div>
+    </main>
 </body>
 
 </html>
