fefabf8cbdf8f754200ef9a5c3bf5a6e81725a01
Marlon Prado <marlonprado04@gmail.com.br>
2024-03-25 22:34:09 -0300

    refactor: restructure folder organization for improved clarity and responsibility separation

--- app/View/Mobile/index.php
+++ app/View/Mobile/index.php
@@ -10,8 +10,7 @@
     <meta http-equiv="Pragma" content="no-cache">
     <meta http-equiv="Expires" content="0">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="styles.css">
-    <link rel="stylesheet" href="index.css">
+    <link rel="stylesheet" href="./src/css/index.css">
     <title>Hamburguerix - Boas-vindas</title>
 </head>
 <body>
@@ -32,6 +31,6 @@
             <button id="buttonMenu">Ver Cardápio ></button>
         </div>
     </main>
-    <script src="index.js"></script>
+    <script src="./src/js/index.js"></script>
 </body>
 </html>
--- app/View/Mobile/menu.php
+++ app/View/Mobile/menu.php
@@ -10,8 +10,7 @@
     <meta http-equiv="Pragma" content="no-cache">
     <meta http-equiv="Expires" content="0">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="/styles.css">
-    <link rel="stylesheet" href="menu.css">
+    <link rel="stylesheet" href="./src/css/menu.css">
     <title>Hamburguerix - Cardápio</title>
 </head>
 <body>
@@ -19,36 +18,36 @@
     <main>
         <div class="top-section">
             <div class="menu-search">
-                <img src="../src/menu-aberto.png" alt="imagem do menu superior">
+                <img src="./src/img/menu-aberto.png" alt="imagem do menu superior">
                 <input type="search" placeholder="Pesquisar" id="inputSearch">
-                <img src="../src/lupa.png" alt="imagem de lupa icone de pesquisa" id="searchIcon">
+                <img src="./src/img/lupa.png" alt="imagem de lupa icone de pesquisa" id="searchIcon">
             </div>
             <h1>Cardápio</h1>
         </div>
         <div class="bottom-section">
             <div class="filter-icons-menu">
-                <img src="../src/hamburguer.png" alt="icone de hamburguer">
-                <img src="../src/french-fries.png" alt="icone de batata-frita">
-                <img src="../src/soda.png" class="width-soda" alt="icone de refrigerante">
+                <img src="./src/img/hamburguer.png" alt="icone de hamburguer">
+                <img src="./src/img/french-fries.png" alt="icone de batata-frita">
+                <img src="./src/img/soda.png" class="width-soda" alt="icone de refrigerante">
             </div>
             <div class="details-item">
-                <img src="../src/hambúrguer-removebg-preview.png" class="image-hamburg" alt="imagem de um hamburguer">
+                <img src="./src/img/hamburguer-removebg-preview.png" class="image-hamburg" alt="imagem de um hamburguer">
                 <div class="text-container">
                     <h2>Nome do hamburguer</h2>
                     <p>Detalhes do hamburguer</p>
                     <h3>R$ 35,90</h3>
                 </div>
-                <img src="../src/carrinho.png" class="icon-add" alt="searchIcon">
+                <img src="./src/img/carrinho.png" class="icon-add" alt="searchIcon">
             </div>
             <div class="bottom-navigation">
-                <img src="../src/carrinho.png" alt="icone de carrinho de compras">
-                <img src="../src/lupa.png" alt="icone de lupa pesquisar" id="searchBottom">
-                <img src="../src/cupom.png" alt="icone de cupom de desconto">
-                <img src="../src/do-utilizador.png" alt="icone de usuario">
+                <img src="./src/img/carrinho.png" alt="icone de carrinho de compras">
+                <img src="./src/img/lupa.png" alt="icone de lupa pesquisar" id="searchBottom">
+                <img src="./src/img/cupom.png" alt="icone de cupom de desconto">
+                <img src="./src/img/do-utilizador.png" alt="icone de usuario">
             </div>
         </div>
     </main>
 
-    <script src="menu.js"></script>
+    <script src="./src/js/menu.js"></script>
 </body>
 </html>
