eae72e15622e71d97bab5a9a9559219080e9c22f
Marlon Prado <marlonprado04@gmail.com.br>
2024-03-25 23:13:22 -0300

    feat: added files to connect to database

---
+++ app/utils/Config.php
@@ -0,0 +1,12 @@
+<?php
+// Definições do DSN (Data Source Name) do banco de dados
+// SGBD: mysql
+// Porta: 3306
+// Banco: hamburguerix
+define("DSN", "mysql:host=localhost:3306;dbname=hamburguerix");
+
+// Define usuário como root (padrão)
+define("USER", "root");
+
+// Define senha como em branco
+define("PASSWORD", "");
